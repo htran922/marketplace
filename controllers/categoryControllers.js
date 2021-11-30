39d6e28a3131ade3e8dbdc9d5d9be29b0ffb994d
Helen <helentran.dev@gmail.com>
2021-11-30 01:12:06 -0500

    Add endpoints for GET category by id and GET categories

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -17,6 +17,7 @@ const categoryControllers = {
 
     const key = datastore.key("Categories");
     const currentDateTime = new Date();
+    const listings = []; // A new category should start off with no listings
     const newCategory = {
       key: key,
       data: {
@@ -24,6 +25,7 @@ const categoryControllers = {
         description,
         created_at: currentDateTime,
         modified_at: currentDateTime,
+        listings,
       },
     };
 
@@ -40,11 +42,108 @@ const categoryControllers = {
           description,
           created_at: currentDateTime,
           modified_at: currentDateTime,
+          listings,
           self,
         });
       }
     });
   },
+
+  /*
+   * View a single category entity by id
+   */
+  getCategory: async (req, res) => {
+    const categoryId = parseInt(req.params.category_id, 10);
+    const key = datastore.key(["Categories", categoryId]);
+
+    const [entity] = await datastore.get(key);
+
+    if (entity) {
+      const { name, description, created_at, modified_at, listings } = entity;
+      const { protocol, originalUrl } = req;
+
+      const self = `${protocol}://${req.get("host")}${originalUrl}`;
+
+      if (listings.length > 0) {
+        listings.forEach((listing) => {
+          const url = `${protocol}://${req.get("host")}/listings/${listing.id}`;
+          listing["self"] = url;
+        });
+      }
+
+      res.status(200).json({
+        id,
+        name,
+        description,
+        created_at,
+        modified_at,
+        listings,
+        self,
+      });
+    } else {
+      res.status(404).json({
+        Error: "No category with this category_id exists",
+      });
+    }
+  },
+
+  /*
+   * Get all category entities with pagination
+   */
+  getCategories: async (req, res) => {
+    const generalQuery = datastore.createQuery("Categories");
+    let paginatedQuery = datastore.createQuery("Categories").limit(5);
+    let response = {};
+
+    const { protocol, baseUrl } = req;
+
+    // If req params contains "cursor" then start query from this id
+    if (Object.keys(req.query).includes("cursor")) {
+      paginatedQuery = paginatedQuery.start(req.query.cursor);
+    }
+
+    // Set the item count in response
+    const results = await datastore.runQuery(generalQuery);
+    response["total_count"] = results[0].length;
+
+    const paginatedResults = await datastore.runQuery(paginatedQuery);
+    const entities = paginatedResults[0];
+    const info = paginatedResults[1];
+
+    // Map the entities to an items property in the response
+    response.items = entities.map(ds.fromDatastore);
+
+    response.items.forEach((entity) => {
+      // Add self link to each category
+      const self = `${protocol}://${req.get("host")}/categories/${entity.id}`;
+      entity["self"] = self;
+
+      // Add self link to associated listings
+      if (entity.listings.length > 0) {
+        entity.listings.forEach((listing) => {
+          const url = `${protocol}://${req.get("host")}/listings/${listing.id}`;
+          listing["self"] = url;
+        });
+      }
+    });
+
+    if (info.moreResults !== ds.Datastore.NO_MORE_RESULTS) {
+      // If there are more results to retrieve return the url for next page
+      response.next = `${protocol}://${req.get(
+        "host"
+      )}${baseUrl}?cursor=${encodeURIComponent(info.endCursor)}`;
+    }
+
+    res.status(200).json(response);
+  },
+
+  /**
+   * Edit a category by id
+   */
+
+  /**
+   * Delete a category by id
+   */
 };
 
 module.exports = categoryControllers;
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const categoryControllers = require("../controllers/categoryControllers");
 
-router.route("/").post(categoryControllers.addCategory);
+router
+  .route("/")
+  .post(categoryControllers.addCategory)
+  .get(categoryControllers.getCategories);
 
 module.exports = router;
